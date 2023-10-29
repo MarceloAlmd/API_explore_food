aefb24f70ea220d43c4f789d06789d75b4dd4276
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-10-29 17:26:39 -0300

    feat: create a new user in database

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,8 +1,28 @@
+const AppError = require("../utils/appError");
+const knex = require("../database/knex");
+const { hash } = require("bcryptjs");
+
 class UsersControllers {
   async create(request, response) {
-    return response.json({
-      status: "sucesso",
-      message: "usuário criado com sucesso",
+    const { name, email, password, role } = request.body;
+
+    const user = await knex("users").where({ email }).first();
+    if (user) {
+      throw new AppError("This email is already in use", 401);
+    }
+
+    const hashPassword = await hash(password, 8);
+
+    await knex("users").insert({
+      name,
+      email,
+      password: hashPassword,
+      role,
+    });
+
+    return response.status(201).json({
+      status: "success",
+      message: "User created successfully",
     });
   }
 }

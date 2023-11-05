29143a7b759078f044df11599a745e8d92bd3fe0
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-11-05 15:58:12 -0300

    feat: route for create user admin by master created

--- src/controllers/masterControllers.js
+++ src/controllers/masterControllers.js
@@ -1,6 +1,30 @@
+const { hash } = require("bcryptjs");
 const knex = require("../database/knex");
-
+const AppError = require("../utils/appError");
 class MasterControllers {
+  async create(request, response) {
+    const { name, email, password, role = "admin" } = request.body;
+
+    const user = await knex("users").where({ email }).first();
+
+    if (user) {
+      throw new AppError("This user is already registered");
+    }
+
+    const hashPassword = await hash(password, 8);
+
+    await knex("users").insert({
+      name,
+      email,
+      password: hashPassword,
+      role,
+    });
+
+    return response.json({
+      message: "administrative user created successfully",
+    });
+  }
+
   async index(request, response) {
     const users = await knex("users");
 
--- src/routes/master.routes.js
+++ src/routes/master.routes.js
@@ -3,5 +3,6 @@ const MasterControllers = require("../controllers/masterControllers");
 const masterRoutes = Router();
 const masterControllers = new MasterControllers();
 masterRoutes.get("/", masterControllers.index);
+masterRoutes.post("/", masterControllers.create);
 
 module.exports = masterRoutes;

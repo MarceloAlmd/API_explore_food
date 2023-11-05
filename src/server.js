5c44bc52123e5e206c8055e93f0a5750a4b44038
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-11-05 11:40:49 -0300

    feat: showing files img

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("express-async-errors");
 const express = require("express");
+const uploadConfig = require("./configs/uploads");
 const routes = require("./routes");
 const serverError = require("./utils/serverError");
 const sqliteConnection = require("./database/sqlite");
@@ -7,6 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 sqliteConnection();
+
+app.use("/files", express.static(uploadConfig.DISHES));
+app.use("/files", express.static(uploadConfig.INGREDIENTS));
 app.use((error, request, response, next) =>
   serverError(error, request, response, next)
 );

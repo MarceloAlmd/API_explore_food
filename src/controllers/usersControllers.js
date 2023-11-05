74f1dd1dad8877af113b94351f439bd89d8db4b7
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2023-11-05 11:59:51 -0300

    feat: update user create

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,6 +1,6 @@
 const AppError = require("../utils/appError");
 const knex = require("../database/knex");
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 
 class UsersControllers {
   async create(request, response) {
@@ -23,6 +23,60 @@ class UsersControllers {
     return response.status(201).json({
       status: "success",
       message: "User created successfully",
+    });
+  }
+
+  async update(request, response) {
+    const { name, email, current_password, new_password } = request.body;
+    const { id } = request.params;
+
+    const user = await knex("users").where({ id }).first();
+
+    if (!user) {
+      throw new AppError("User not found");
+    }
+
+    if (email) {
+      const checkEmail = await knex("users").where({ email }).first();
+      if (checkEmail && checkEmail.id !== user.id) {
+        throw new AppError("This Email is already in use.");
+      }
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if (current_password && !new_password) {
+      throw new AppError("Enter your new password");
+    }
+
+    if (!current_password && new_password) {
+      throw new AppError("Enter your current password");
+    }
+
+    if (current_password && new_password) {
+      const checkCurrentPassword = await compare(
+        current_password,
+        user.password
+      );
+
+      if (!checkCurrentPassword) {
+        throw new AppError("The current password is invalid");
+      }
+
+      user.password = await hash(new_password, 8);
+    }
+
+    await knex("users").where({ id }).update({
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      updated_at: knex.fn.now(),
+    });
+
+    return response.json({
+      message: "User updated successfully",
+      user,
     });
   }
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,5 +5,6 @@ const usersRoutes = Router();
 const usersControllers = new UsersControllers();
 
 usersRoutes.post("/", usersControllers.create);
+usersRoutes.put("/:id", usersControllers.update);
 
 module.exports = usersRoutes;

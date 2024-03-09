fd9c3d4113ffd4d2924b0c6fb710e1f37d4e9d4e
Marcelo Magalhaes <marcelo3x4@hotmail.com>
2024-03-09 14:38:10 -0300

    fix: change folder name

--- src/configs/uploads.js
+++ src/configs/uploads.js
@@ -3,8 +3,8 @@ const crypto = require("crypto");
 const multer = require("multer");
 
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
-const DISHES = path.resolve(TMP_FOLDER, "DISHES");
-const INGREDIENTS = path.resolve(TMP_FOLDER, "INGREDIENTS");
+const DISHES = path.resolve(TMP_FOLDER, "DishesIMG");
+const INGREDIENTS = path.resolve(TMP_FOLDER, "IngredientsIMG");
 
 console.log("TMP_FOLDER", TMP_FOLDER);
 console.log("DISHES", DISHES);
